48a23f1aecffea77d718b5fc737a23680bb850b4
Joonyoung Park <jooony@snu.ac.kr>
2024-03-18 17:43:37 +0900

    [main] Finished profile section

---
+++ src/lib/components/Profile.svelte
@@ -0,0 +1,68 @@
+<script>
+	import profile from '$lib/images/profile.png';
+	import email from '$lib/images/email.svg';
+	import github from '$lib/images/github-mark.svg';
+	import instagram from '$lib/images/instagram_mark.svg';
+</script>
+
+<!-- Profile pic, name and social buttons -->
+<div class="flex flex-col items-center mb-8">
+	<!-- Profile pic -->
+	<img alt="profile pic" src={profile} class="w-36 rounded-full" />
+	<!-- Name -->
+	<h1 class="font-semibold font-biryani text-xl my-1">Joonyoung Park</h1>
+	<!-- Social Buttons -->
+	<div class="flex">
+		<!-- mail -->
+		<a href="mailto:jooony@snu.ac.kr" target="_blank" rel="noopener noreferrer" class="w-6 mr-2">
+			<img alt="email" src={email} />
+		</a>
+		<!-- github -->
+		<a href="https://github.com/partlyjadedyouth" target="_blank" rel="noopener noreferrer">
+			<img alt="github" src={github} class="w-6 mr-2" />
+		</a>
+		<!-- instagram -->
+		<a href="https://instagram.com/partlyjadedyouth" target="_blank" rel="noopener noreferrer">
+			<img alt="instagram" src={instagram} class="w-6" />
+		</a>
+	</div>
+</div>
+
+<!-- Self-introduction -->
+<div class="flex flex-col font-barlow text-sm">
+	<p class="mb-3">
+		I am an aspiring HCI researcher and undergraduate student in
+		<a
+			href="https://snu.ac.kr"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="font-medium hover:underline"
+		>
+			Seoul National University.
+		</a>
+
+		I am majoring in
+		<a
+			href="https://ece.snu.ac.kr"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="font-medium hover:underline"
+		>
+			Electrical & Computer Engineering,
+		</a>
+		and
+		<a
+			href="https://isc.snu.ac.kr"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="font-medium hover:underline"
+		>
+			Information Science & Cultural Studies.
+		</a>
+	</p>
+	<p>
+		My research interest lies in the intersection of human-computer interaction (HCI), human-AI
+		interaction (HAI), and natural language processing (NLP). Specifically, I want to find ways to
+		improve the interaction and accessibility of computers for marginalized people.
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-	import profile from '$lib/images/profile.png';
-	import email from '$lib/images/email.svg';
-	import github from '$lib/images/github-mark.svg';
-	import instagram from '$lib/images/instagram_mark.svg';
+	import Profile from '$lib/components/Profile.svelte';
 </script>
 
 <svelte:head>
@@ -10,27 +7,12 @@
 	<meta name="description" content="Joonyoung's Blog" />
 </svelte:head>
 
-<section>
-	<!-- Profile pic, name and social buttons -->
-	<div class="flex flex-col items-center py-14">
-		<!-- Profile pic -->
-		<img alt="profile pic" src={profile} class="w-36 rounded-full" />
-		<!-- Name -->
-		<h1 class="font-semibold font-biryani text-xl my-1">Joonyoung Park</h1>
-		<!-- Social Buttons -->
-		<div class="flex">
-			<!-- mail -->
-			<a href="mailto:jooony@snu.ac.kr" target="_blank" rel="noopener noreferrer" class="w-6 mr-2">
-				<img alt="email" src={email} />
-			</a>
-			<!-- github -->
-			<a href="https://github.com/partlyjadedyouth" target="_blank" rel="noopener noreferrer">
-				<img alt="github" src={github} class="w-6 mr-2" />
-			</a>
-			<!-- instagram -->
-			<a href="https://instagram.com/partlyjadedyouth" target="_blank" rel="noopener noreferrer">
-				<img alt="instagram" src={instagram} class="w-6" />
-			</a>
-		</div>
-	</div>
+<section class="py-10">
+	<!-- Profile section -->
+	<Profile />
+</section>
+
+<section class="py-10">
+	<!-- Latest Projects section -->
+	<h1 class="font-barlow font-medium text-xl">Latest Projects</h1>
 </section>
