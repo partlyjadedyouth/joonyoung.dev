c372e0cb82bf811b516a7a03b4f86692e3eee1d9
Joonyoung Park <jooony@snu.ac.kr>
2024-04-15 14:01:23 +0900

    [main] Add assignment2 route

---
+++ src/routes/(assignment)/assignment2/+page.svelte
@@ -0,0 +1,124 @@
+<script>
+</script>
+
+<header>
+	<div class="logo">
+		<img src={`/assignment2/isc-copy.png`} alt="ISC News" />
+		<a style="text-decoration: none;" href="https://isc.snu.ac.kr"><div>ISC NEWS</div></a>
+	</div>
+</header>
+
+<article>
+	<h1>"계속 같은 대답만"..AI 검색 조작 속에 숨겨진 바이럴 마케팅</h1>
+	<h2>입력: 2024-04-15 13:10 | 수정: 2024-04-15 15:57</h2>
+	<div class="content">
+		<img src={`/assignment2/people.png`} alt="ISC News" class="centered-image" />
+		<br />
+		<strong>(앵커)</strong> <br />
+		AI 검색을 통해 맛집이나 카페 뿐만 아니라 영화나 음악까지 추천받는 분들 많으시죠.
+		<br /><br />
+		그런데 요즘 특정 검색 결과가 반복해서 나타나는 현상이 발생하면서 불편을 호소하는 일이 늘어나고 있습니다.
+		<br /><br />
+		취재 결과, 이는 AI의 알고리즘을 공략한 '바이럴 마케팅’ 때문이라고 합니다. 김정문 기자입니다.
+		<br />
+		<br />
+		<br /><strong>(리포트)</strong><br />
+		네이버의 AI 검색 서비스인 큐(Cue)에 '여의도 맛집’을 추천해달라고 하자, 세 곳의 양식당을 추천해줍니다.
+		<br /><br />
+		그런데 '여의도 한식 맛집', '여의도 일식 맛집’처럼 이들과 전혀 관계 없는 키워드로 검색을 해도 세 식당
+		중 최소 한 곳은 검색 결과에 나타납니다.
+		<br /><br />
+		'여의도’과 '맛집’만 들어가면 이들 식당의 이름을 확인할 수 있습니다.
+		<br /><br />영화나 음악도 마찬가지입니다.
+		<br /><br />이는 바이럴 마케팅 업체가 AI 알고리즘의 허점을 이용해 학습 데이터로 의뢰인이 원하는
+		내용이 사용될 수 있도록 유도하였기 때문입니다.
+		<br /><br />AI 바이럴 마케팅은 그동안 암암리에 이루어져 온 것으로 보이나 입소문을 타고 그 수가
+		점차 많아졌습니다.<br /><br />
+		이들은 영향력 있는 SNS 계정들을 이용하여 특정 내용을 지속적으로 공유하는 방식을 통해 신뢰도를 높이면
+		AI가 학습할 확률이 높아지고, 이를 통해 AI의 검색 결과를 바꿀 수 있다고 주장합니다.
+		<br /><br />
+		[바이럴 마케팅 업체 관계자]
+		<br />생성AI는 신뢰도가 중요해서, 알고리즘으로 한번 필터링을 한 다음에 데이터로 등록을
+		시키거든요. 그래서 저희 SNS 계정으로 계속 글을 올리면 데이터의 신뢰도가 높아져서 검색 결과가
+		바뀌는…
+		<br /><br />자세한 작업 기간과 가격도 들을 수 있었습니다.
+		<br /><br />[바이럴 마케팅 업체 관계자]
+		<br />검색 결과는 늦어도 6개월 이내에 반영되고요. 네이버에만 올리는 건 백만 원부터 시작하고
+		챗지피티(ChatGPT)나 제미나이(Gemini)까지 올리려면 2천만 원 이상은 생각하셔야 해요.
+		<br /><br />신뢰할 수 없는 검색 결과에 이용자들의 불만도 커지고 있습니다.
+		<br /><br />
+		[문보정 / 시민]<br /> 처음에는 검색 결과가 믿을 만해서 자주 사용했는데, 요즘은 자꾸 같은 것만
+		추천해줘서 이걸 믿어도 되나 싶고… <br /><br />네이버 측은 보도자료를 통해 학습 데이터 선별
+		알고리즘을 수정하고, 문제가 되는 데이터를 삭제하겠다고 밝혔습니다. 구글 역시 비정상적인 데이터
+		조작이 의심되는 경우에는 불이익을 주겠다는 입장입니다.<br /><br /> 그러나 AI 전문가들은 이러한
+		조치에 회의적입니다.<br /><br />
+		[임문가 / 서울대학교 컴퓨터공학부 교수]<br />이미 AI 모델에 조작된 정보가 많이 학습되어 모델의
+		가중치에 영향을 준 상태이기 때문에, 지금 당장 데이터를 삭제하더라도 그것이 반영되는 데에는 오랜
+		시간이 걸릴 것입니다. 또한 다른 허점이 발견된다면 같은 일이 계속해서 반복될 것이기 때문에…
+		<br /><br />
+		바이럴 마케팅 논란에 신뢰를 잃어가고 있는 AI 검색 서비스. <br /><br />AI 시대로의 걸림돌이
+		되지는 않을지 우려를 모으고 있습니다. <br /><br />ISC 뉴스 김정문입니다.
+		<br /><br />
+		<strong>ISC 뉴스는 24시간 여러분의 제보를 기다립니다.</strong>
+		<br /><br />
+		▷ 전화 02-123-4567<br />
+		▷ 이메일 iscjebo@isc.co.kr<br />
+		▷ 카카오톡 @iscjebo<br />
+	</div>
+</article>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap');
+
+	a body {
+		font-family: 'Merriweather', serif;
+		line-height: 1.6;
+		background: #f4f4f4;
+		margin: 0;
+		padding: 0;
+	}
+	header {
+		background-color: orange;
+		color: black;
+		text-align: center;
+		padding: 10px 0;
+	}
+	.logo img {
+		width: 80px; /* or any size that fits your design */
+		height: auto; /* maintains the aspect ratio */
+	}
+	.logo {
+		font-family: 'Merriweather';
+		font-size: 35px;
+		font-weight: bold;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	article {
+		max-width: 800px;
+		margin: 10px auto;
+		background: white;
+		padding: 20px;
+		box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
+		border-radius: 8px;
+	}
+	h1 {
+		font-size: 28px;
+		font-weight: bold;
+	}
+	h2 {
+		font-size: 16px;
+		font-weight: normal;
+		color: #666;
+		margin-bottom: 20px;
+	}
+	.content {
+		margin-top: 30px;
+	}
+	.centered-image {
+		display: block;
+		margin: 20px auto; /* Centers the image and adds space above and below */
+		width: 80%; /* Adjust width as necessary */
+	}
+</style>
