12b940ef8621b5bb748fa64852f3154825f6258f
Joonyoung Park <jooony@snu.ac.kr>
2024-05-16 00:26:26 +0900

    [main] refactor: Update project title and description for Wavin

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -53,9 +53,9 @@ const togetherBus: Project = {
 const wavin: Project = {
 	id: 'wavin',
 	year: '2024',
-	title: 'Wavin',
-	description:
-		'A study on lyrics visualization in mobile music streaming services to improve music comprehension for D/HH',
+	title:
+		'Wavin: Lyrics Visualization in Mobile Music Streaming Services to Improve Music Comprehension for D/HH',
+	description: '',
 	thumbnail: '/images/wavin.png',
 	role: 'Project leader',
 	post: 'Many people believe that D/HH cannot enjoy music, but this is not true. A lots of D/HH have a desire to enjoy music in their daily lives through mobile music streaming applications, but they fell it hard to understand music because those apps are designed for hearing people (Fourney, 2015). <br /><br /> The purpose of this study is to improve D/HH usability of mobile music streaming services from an HCI perspective, by identifying specific elements that should be included in lyrics visualizations to improve music comprehension for D/HH, and presenting design guidelines for a lyrics display UI based on these elements. <br /><br /> In this study, we conducted in-depth interviews with D/HH to find out the problems they experience with mobile music streaming services. As a result, we proposed and evaluated visualization of lyrics as a way to solve them. The proposed UI prototype, Wavin, shows which letter is currently being sung by the singer, places each letter at a different height on the display depending on the pitch, and displays the name of the singer. In prototype testing, study participants rated Wavin as helping them understand music better than traditional lyric display UIs. <br /><br /> This design work was submitted as coursework for Human-Computer Interaction course at Dept. of Information Science, Seoul National University.',
