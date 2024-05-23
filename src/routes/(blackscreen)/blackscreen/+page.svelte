c7111e9fa70fe0591a20dc83fd867b1786b3b62a
Joonyoung Park <jooony@snu.ac.kr>
2024-05-23 10:08:41 +0900

    [main] Add blackscreen

---
+++ src/routes/(blackscreen)/blackscreen/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>let's clean the display</title>
+	<meta name="description" content="Joonyoung's Blog" />
+</svelte:head>
+
+<div class="bg-black h-screen flex items-center justify-center"></div>
